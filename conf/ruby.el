82fda32f56212711dba3e8d12c998ac75cb3ecce
iori <matsuhar@gmail.com>
2013-11-25 12:00:22 +0100

    Enhanced ruby mode install via package

--- conf/ruby.el
+++ conf/ruby.el
@@ -1,6 +1,5 @@
 ;; Enhanced Ruby Mode
-(add-to-list 'load-path 
-             "~/.emacs.d/plugins/Enhanced-Ruby-Mode")
+(install-package 'ruby-mode)
 (require 'ruby-mode)
 
 (defun set-newline-and-indent ()
@@ -8,18 +7,14 @@
 (add-hook 'ruby-mode-hook 'set-newline-and-indent)
 
 ;; HAML mode
+(install-package 'haml-mode)
 (add-hook 'haml-mode-hook
           (lambda ()
             (setq indent-tabs-mode nil)
             (local-set-key (kbd "RET") 'newline-and-indent)))
 
-(defun install-package (name)  
-  (unless (package-installed-p name)
-    (package-refresh-contents) (package-install name)))
-
 ;; Ruby electric mode
 (install-package 'ruby-electric)
-
 (defun my-ruby-mode-hook ()
   (ruby-electric-mode))
 (add-hook 'ruby-mode-hook 'my-ruby-mode-hook) 
