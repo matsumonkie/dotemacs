590e7c1236c35992fd13f940de06dfcd17b0b7d9
Iori Matsuhara <matsuhar@gmail.com>
2013-12-27 21:55:43 +0100

    format reset.el with shorter syntax

--- home/.emacs.d/conf/reset.el
+++ home/.emacs.d/conf/reset.el
@@ -1,68 +1,25 @@
 ;;
-;; CTRL + ...
+;; CTRL
 ;;
 
-(global-unset-key "\C-a") 
-(global-unset-key "\C-b")
-;(global-unset-key "\C-c")
-(global-unset-key "\C-d")
-(global-unset-key "\C-e")
-(global-unset-key "\C-f")
-(global-unset-key "\C-g")
-;;(global-unset-key "\C-h") aide
-;(global-unset-key "\C-i") associé par le système a tabulation
-(global-unset-key "\C-j")
-(global-unset-key "\C-k")
-;(global-unset-key "\C-l") recenter-top-bottom
-;;(global-unset-key "\C-m") empeche le fonctionnement de la touche entrée en console
-(global-unset-key "\C-n")
-(global-unset-key "\C-o")
-(global-unset-key "\C-p")
-(global-unset-key "\C-q")
-(global-unset-key "\C-r")
-(global-unset-key "\C-s")
-(global-unset-key "\C-t")
-(global-unset-key "\C-u")
-(global-unset-key "\C-v")
-(global-unset-key "\C-w")
-(global-unset-key "\C-x")
-(global-unset-key "\C-y")
-(global-unset-key "\C-z")
-
-(global-unset-key (kbd "C-SPC"))
-
+;; do not unset "\C-m" otherwise enter key does not work in console mode
+(dolist (key '(
+"\C-a" "\C-b" "\C-d" "\C-e" "\C-f" "\C-g"
+"\C-j" "\C-k" "\C-l" "\C-n" "\C-o" "\C-p"
+"\C-q" "\C-r" "\C-s" "\C-t" "\C-u" "\C-v"
+"\C-w" "\C-x" "\C-y" "\C-z" "\C-SPC"))
+(global-unset-key key))
 
 ;;
-;; META + ...
+;; META
 ;;
-
-
-(global-unset-key "\M-a")
-(global-unset-key "\M-b")
-(global-unset-key "\M-c")
-(global-unset-key "\M-d")
-(global-unset-key "\M-e")
-(global-unset-key "\M-f")
-(global-unset-key "\M-g")
-(global-unset-key "\M-h")
-;;(global-unset-key "\M-i")
-(global-unset-key "\M-j")
-(global-unset-key "\M-k")
-(global-unset-key "\M-l")
-(global-unset-key "\M-m")
-(global-unset-key "\M-n")
-(global-unset-key "\M-o")
-(global-unset-key "\M-p")
-(global-unset-key "\M-q")
-(global-unset-key "\M-r")
-(global-unset-key "\M-s")
-(global-unset-key "\M-t")
-(global-unset-key "\M-u")
-(global-unset-key "\M-v")
-(global-unset-key "\M-w")
-;;(global-unset-key "\M-x")
-(global-unset-key "\M-y")
-(global-unset-key "\M-z")
+(dolist (key '(
+  "\M-a" "\M-b" "\M-c" "\M-d" "\M-e" "\M-f"
+  "\M-g" "\M-h" "\M-j" "\M-k" "\M-l" "\M-m"
+  "\M-n" "\M-o" "\M-p" "\M-q" "\M-r" "\M-s"
+  "\M-t" "\M-u" "\M-v" "\M-w" "\M-x" "\M-y"
+  "\M-z" "\M-SPC"))
+(global-unset-key key))
 
 (global-unset-key "\C-e\C-n")
 
