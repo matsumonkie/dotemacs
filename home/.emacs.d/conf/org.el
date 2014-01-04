0173e4e7bfedd326f718b682b12fc14398d09a4f
Iori Matsuhara <matsuhar@gmail.com>
2014-01-04 21:20:08 +0100

    ++

--- home/.emacs.d/conf/org.el
+++ home/.emacs.d/conf/org.el
@@ -4,6 +4,13 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 
+; org personnal hook
+(defun my-org-hook ()
+  (local-set-key (kbd "C-e") 'set-mark-command)
+)
+
+(add-hook 'org-mode-hook 'my-org-hook)
+
 ; agenda-files
 (setq org-agenda-files (list "~/.org/perso.org"
                              "~/.org/selectra.org"
